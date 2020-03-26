bc88c8f03993fdd50015f7e4ad2b67c27260f22a
hAislt <tranh@dhbw-loerrach.de>
2020-03-26 14:47:47 +0100

    update
    additional components

--- SQL_EXPORT/shop.sql
+++ SQL_EXPORT/shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 25. Mrz 2020 um 12:14
+-- Erstellungszeit: 26. Mrz 2020 um 14:25
 -- Server-Version: 10.4.11-MariaDB
 -- PHP-Version: 7.4.3
 
@@ -25,28 +25,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `cards`
+--
+
+CREATE TABLE `cards` (
+  `id` int(11) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `products`
 --
 
 CREATE TABLE `products` (
   `id` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `beschreibung` text NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `beschreibung` text CHARACTER SET utf8 NOT NULL,
+  `bescheibung_kurz` text CHARACTER SET utf8 NOT NULL,
   `preis` double NOT NULL,
-  `bild` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `bild` text CHARACTER SET utf8 NOT NULL,
+  `link` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Daten für Tabelle `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `beschreibung`, `preis`, `bild`) VALUES
-(1, '6GB MSI GeForce GTX 1660 ARMOR 6G OC Aktiv PCIe 3.0 x16 (Retail)', 'Seit dem 14.03.2019 im Sortiment\r\n\r\nTURING-SHADER\r\nTuring-Shader ermöglichen beeindruckende Leistungssteigerungen für aktuelle Spiele durch die gleichzeitige Ausführung von Gleitkomma- und Ganzzahloperationen, adaptive Shading-Technik und einer neuen Speicherarchitektur mit doppelt so großem Cache im Vergleich zu Vorgänger-Grafikkarten.\r\n\r\nSTARKE PERFORMANCE\r\nSei bereit für aktuelle Spiele mit der GeForce GTX 1660 Ti, die auf einem Level mit der GeForce GTX 1070 steht.\r\n\r\nGEFORCE EXPERIENCE\r\nFange dein Spielerlebnis mit Videos und Screenshots ein und teile es mit der Welt, auch in einem Livestream. Halte deine GeForce-Treiber auf dem neusten Stand und optimiere deine Spiele-Einstellungen. GeForce Experience™ ist eine essentielle Software für die Verwendung deiner GeForce-Grafikkarte.\r\n\r\nDIE ARMOR-SERIE\r\nStylisch zocken und sich von der Masse abheben - beides wird von unseren ARMOR-Modellen ermöglicht. ARMOR-Grafikkarten sind die perfekten Komponenten für Spieler und Case-Modder, die auf der Suche nach etwas anderem sind. Hier trifft Gaming auf Klasse.\r\n\r\nDAS ARMOR-KÜHLSYSTEM\r\nZwei preisgekrönte TORX 2.0 Lüfter mit Doppelkugellager machen das Kühlsystem der ARMOR-Grafikkarten zu einer überaus zuverlässigen Kühllösung für lange Gaming-Sessions.\r\n\r\nBRILLIANZ AUF DER SPUR\r\nUnter all diesem Material befinden sich mehrschichtige Leiterbahnen, die das Printed Circuit Board ausmachen. Diese Bahnen verbinden alle wichtigen Komponenten miteinander und ermöglichen diesen die blitzschnelle Kommunikation untereinander.\r\n\r\nDIE BESTE ERFAHRUNG\r\nHole das Beste aus deiner MSI Grafikkarte heraus. Die verfügbare Software bietet unzählige Anpassungsmöglichkeiten, um die gewünschte Leistung in Spielen zu sichern.', 241.99, '1300282_0__72667.jpg'),
-(2, '4GB MSI GeForce GTX 1050 Ti GAMING X 4G Aktiv PCIe 3.0 x16', 'Seit dem 22.10.2016 im Sortiment\r\n\r\nMSI präsentiert mit der GeForce® GTX 1050 Ti GAMING X 4G eine Grafikkarte, die beste Leistung mit modernster Technik in einem optimalen Gamingumfeld kombiniert. Die Grafikkarte verfügt über die moderne MSI TORX 2.0 Technik. Mithilfe dieser erklimmt TWIN FROZR VI eine neue Leistungsebene der Grafikkartenkühlung. TORX 2.0 Kühler generieren 22% mehr Luftdruck und sind zugleich leiser.\r\n\r\nUnter dem Cover der GeForce® GTX 1050 Ti GAMING X 4G befindet sich ein Meisterstück der Ingenieurskunst, das dazu entwickelt wurde die Grafikkarte absolut kühl zu halten. Jedes einzelne Detail des komplizierten Kühlkörpers trägt dazu bei, ein kühles und leises Spielerlebnis zu bieten. Durch den Einsatz fortschrittlicher Aerodynamik lenkt die Luftstrom-Kontrolltechnik mehr Luft zu den TWIN FROZR VI Heatpipes. Die speziellen Ableiter auf dem Kühlkörper vergrößern dessen Oberfläche, was sowohl die Temperatur als auch die Leistung spürbar positiv beeinflusst.\r\n\r\nErleben Sie die Zukunft des Gamings. So unterstütz die innovative Grafikkarte von MSI unter anderem DirectX 12. Dank der NVIDIA® G-Sync™-Technologie stehen Ihnen bedeutet die flüssigsten, schnellsten und aufregendsten Gaming-Erlebnisse bevor. Screen-Tearing, Bildruckeln oder Input-Lags gehören mit der GeForce® GTX 1050 Ti GAMING X 4G der Vergangenheit an. Freuen Sie sich stattdessen auf Ultra-High Definition (UHD) und eine weitaus schärfere und realitätsgetreuere Detaildarstellung in Games.', 148.99, '1125753_0__68857.jpg');
+INSERT INTO `products` (`id`, `name`, `beschreibung`, `bescheibung_kurz`, `preis`, `bild`, `link`) VALUES
+(1, '6GB MSI GeForce GTX 1660 ARMOR 6G OC Aktiv PCIe 3.0 x16 (Retail)', 'Seit dem 14.03.2019 im Sortiment\r\n\r\nTURING-SHADER\r\nTuring-Shader ermöglichen beeindruckende Leistungssteigerungen für aktuelle Spiele durch die gleichzeitige Ausführung von Gleitkomma- und Ganzzahloperationen, adaptive Shading-Technik und einer neuen Speicherarchitektur mit doppelt so großem Cache im Vergleich zu Vorgänger-Grafikkarten.\r\n\r\nSTARKE PERFORMANCE\r\nSei bereit für aktuelle Spiele mit der GeForce GTX 1660 Ti, die auf einem Level mit der GeForce GTX 1070 steht.\r\n\r\nGEFORCE EXPERIENCE\r\nFange dein Spielerlebnis mit Videos und Screenshots ein und teile es mit der Welt, auch in einem Livestream. Halte deine GeForce-Treiber auf dem neusten Stand und optimiere deine Spiele-Einstellungen. GeForce Experience™ ist eine essentielle Software für die Verwendung deiner GeForce-Grafikkarte.\r\n\r\nDIE ARMOR-SERIE\r\nStylisch zocken und sich von der Masse abheben - beides wird von unseren ARMOR-Modellen ermöglicht. ARMOR-Grafikkarten sind die perfekten Komponenten für Spieler und Case-Modder, die auf der Suche nach etwas anderem sind. Hier trifft Gaming auf Klasse.\r\n\r\nDAS ARMOR-KÜHLSYSTEM\r\nZwei preisgekrönte TORX 2.0 Lüfter mit Doppelkugellager machen das Kühlsystem der ARMOR-Grafikkarten zu einer überaus zuverlässigen Kühllösung für lange Gaming-Sessions.\r\n\r\nBRILLIANZ AUF DER SPUR\r\nUnter all diesem Material befinden sich mehrschichtige Leiterbahnen, die das Printed Circuit Board ausmachen. Diese Bahnen verbinden alle wichtigen Komponenten miteinander und ermöglichen diesen die blitzschnelle Kommunikation untereinander.\r\n\r\nDIE BESTE ERFAHRUNG\r\nHole das Beste aus deiner MSI Grafikkarte heraus. Die verfügbare Software bietet unzählige Anpassungsmöglichkeiten, um die gewünschte Leistung in Spielen zu sichern.', 'GPU Modell: 	GeForce GTX 1050 Ti\r\nEdition: 	Gaming X 4G\r\nCodename: 	GP107-400-A1\r\nSchnittstelle: 	PCIe 3.0 x16\r\nGPU Anzahl: 	Single GPU\r\nGPU Takt: 	1379MHz\r\nBoost Takt: 	1493MHz', 241.99, '1300282_0__72667.jpg', 'gtx1.php'),
+(2, '4GB MSI GeForce GTX 1050 Ti GAMING X 4G Aktiv PCIe 3.0 x16', 'Seit dem 22.10.2016 im Sortiment\r\n\r\nMSI präsentiert mit der GeForce® GTX 1050 Ti GAMING X 4G eine Grafikkarte, die beste Leistung mit modernster Technik in einem optimalen Gamingumfeld kombiniert. Die Grafikkarte verfügt über die moderne MSI TORX 2.0 Technik. Mithilfe dieser erklimmt TWIN FROZR VI eine neue Leistungsebene der Grafikkartenkühlung. TORX 2.0 Kühler generieren 22% mehr Luftdruck und sind zugleich leiser.\r\n\r\nUnter dem Cover der GeForce® GTX 1050 Ti GAMING X 4G befindet sich ein Meisterstück der Ingenieurskunst, das dazu entwickelt wurde die Grafikkarte absolut kühl zu halten. Jedes einzelne Detail des komplizierten Kühlkörpers trägt dazu bei, ein kühles und leises Spielerlebnis zu bieten. Durch den Einsatz fortschrittlicher Aerodynamik lenkt die Luftstrom-Kontrolltechnik mehr Luft zu den TWIN FROZR VI Heatpipes. Die speziellen Ableiter auf dem Kühlkörper vergrößern dessen Oberfläche, was sowohl die Temperatur als auch die Leistung spürbar positiv beeinflusst.\r\n\r\nErleben Sie die Zukunft des Gamings. So unterstütz die innovative Grafikkarte von MSI unter anderem DirectX 12. Dank der NVIDIA® G-Sync™-Technologie stehen Ihnen bedeutet die flüssigsten, schnellsten und aufregendsten Gaming-Erlebnisse bevor. Screen-Tearing, Bildruckeln oder Input-Lags gehören mit der GeForce® GTX 1050 Ti GAMING X 4G der Vergangenheit an. Freuen Sie sich stattdessen auf Ultra-High Definition (UHD) und eine weitaus schärfere und realitätsgetreuere Detaildarstellung in Games.', 'GPU Modell: 	GeForce RTX 2060\r\nEdition: 	Gaming Z\r\nCodename: 	TU106\r\nSchnittstelle: 	PCIe 3.0 x16\r\nGPU Anzahl: 	Single GPU\r\nGPU Takt: 	1365MHz\r\nBoost Takt: 	1830MHz\r\nShader Model: 	6.3', 148.99, '1125753_0__68857.jpg', 'gtx2.php'),
+(3, '16GB G.Skill Aegis DDR4-2666 DIMM CL19 Dual Kit', 'G.Skill - leistungsfähige Grafikkarten\r\n\r\nDer taiwanesische Hersteller G.Skill wurde 1989 gegründet und hat sich vor allem auf die Produktion von hochwertigem Arbeitsspeicher und auf überaus funktionale Gaming-Mäuse und Tastaturen spezialisiert. Ganz gleich, ob Gaming-Experte, Content Creator oder PC-Building-Enthusiast: G.Skill überzeugt mit unschlagbarer Performance und dem Willen, das Unternehmen und die eigenen Produkte stetig weiter zu verbessern.', 'Modellname: 	Aegis\r\nGesamtkapazität: 	16GB\r\nAnzahl der Module: 	2x\r\nKapazität der Einzelmodule: 	8192MB\r\nArt des Speichers: 	DDR4-2666\r\nJEDEC Norm: 	PC4-21300U\r\nSpeichertyp: 	unbuffered\r\nBauform: 	DIMM\r\nSpeicherinterface: 	DDR4', 74.99, '1237318_0__8850603.jpg', 'ram.php'),
+(4, 'AMD Ryzen 3 3200G 4x 3.60GHz So.AM4 BOX', 'Seit dem 04.06.2019 im Sortiment\r\n\r\nBewährte Technologien und starke Leistung mit dem CPU AMD Ryzen™ 3 3200G.\r\n\r\nAMD Ryzen™ Prozessoren der 3. Generation mit Radeon™ Grafikeinheit kombinieren die Leistung von Zen+\' und Vega\', um anspruchsvollsten Gamern beeindruckende 1080p-Spielerlebnisse zu bieten, alles auf nur einem Prozessor. Nach dem Spielen erledigen Sie Arbeiten leichter und schneller dank AMD Ryzen™ Prozessoren der 2. Genaration mit Radeon™ Grafikeinheit. Office-Apps, Fotobearbeitung, Surfen im Web und Videostreaming - ein Kinderspiel dank überragender Multithread-Verarbeitung. Ryzen™ Prozessoren mit Radeon™ Grafikeinheit liefern hohe Leistung, die man sehen und fühlen kann.\r\n\r\nAls Teil der Ryzen™ 3000 Serie enthalten die AMD Ryzen™ Desktop Prozessoren eine leistungsstarke und schnelle Radeon™ Grafikeinheit. Die Kombination aus starken Ryzen™ Prozessoren und leistungsfähiger Radeon™ Grafikeinheit ermöglicht Hochleistungs-Gaming ohne separate Grafikkarte, auf den Millionen PC-Spieler weltweit vertrauen.\r\n\r\nDie Ryzen™ Desktop-Prozessoren der 3. Generation basieren auf der 7nm Zen 2-Architektur und bieten mehr Kerne, höhere Taktraten und eine verbesserte Energieeffizienz. Weitere Features von Ryzen™ 3000 sind AMD GameCache, Precision Boost 2 oder die AMD StoreMI Technologie.', 'Kerne/Threads 	4/4\r\nMax. Boost** 	Bis zu 4,0 GHz\r\nCache 	6 MB\r\nTDP 	65 W\r\nPCIe-Version 	3.0\r\nFür Übertaktung freigeschaltet1 	Ja\r\nKühlung 	Wraith Stealth\r\nSockel 	AM4', 114.99, '1313641_0__73060.jpg', 'cpu.php');
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `cards`
+--
+ALTER TABLE `cards`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_id_2` (`product_id`,`user_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indizes für die Tabelle `products`
@@ -59,10 +84,16 @@ ALTER TABLE `products`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `cards`
+--
+ALTER TABLE `cards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT für Tabelle `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ gtx2.php
@@ -0,0 +1,221 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors','On');
+
+$userId =random_int(0,time());
+$cardItems=0;
+ 
+$mysqli = new mysqli('localhost', 'root', '', 'shop');
+if($mysqli->connect_error) {
+  echo 'Fehler bei der Verbindung: ' . mysqli_connect_error();
+  exit();
+  }
+  if(!$mysqli->set_charset('utf8')) {
+  echo 'Fehler beim Laden von UTF-8: ' . mysqli_error();
+  }
+  $sql = 'SELECT * FROM products Where id=2';
+  $result = $mysqli->query($sql);
+
+  $row = $result->fetch_array(MYSQLI_ASSOC);
+
+  if(isset($_COOKIE['userId'])){
+    $userId = (int) $_COOKIE['userId'];
+    }
+  if(isset($_SESSION['userId'])){
+      $userId = (int) $_SESSION['userId'];
+  }
+
+    setcookie('userId',$userId,strtotime('+30 days'));
+
+    $sql ="SELECT  count(id)  FROM cards Where user_id=".$userId;
+    $resultcard = $mysqli->query($sql);
+    $cardItems= (int)$resultcard;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="WebShop">
+    <meta name="author" content="Mario">
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/heroic-features.css" rel="stylesheet">
+    
+    <title>Web Shop</title>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="js/bilder.js" type="text/javascript"></script>
+    <script src="js/zaehler.js" type="text/javascript"></script>
+    
+  </head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Web Shop</a>
+      <div class="collapse navbar-collapse" id="navbarMenu">
+      <div class="input-group md-form form-sm form-2 pl-0">
+        <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <span class="input-group-text red lighten-3" id="basic-text1"><i class="fa fa-search" aria-hidden="true"></i>
+          </span>
+        </div>
+      </div>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item ">
+            <a class="nav-link" href="index.php">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="productsMain.php">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cart.php">Cart(<?php echo $cardItems ?>)</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<div class="container">
+
+  <!-- Portfolio Item Heading -->
+  <h1 class="my-4">Produkt
+    <small></small>
+  </h1>
+
+  <!-- Portfolio Item Row -->
+  <div class="row">
+
+    <div class="col-md-6">
+        <div class="container">
+            <div class="row">
+              
+            <?php for ($i=0; $i < 6; $i++):?>            
+              <div class="mySlides">
+                <div class="numbertext">1 / 6</div>
+                <img src="img/<?php echo $row['bild']; ?>" style="width:100%">
+              </div>
+            <?php endfor?>
+              
+              <a class="prev" onclick="plusSlides(-1)">❮</a>
+              <a class="next" onclick="plusSlides(1)">❯</a>
+            </div>
+            <div class="caption-container row ">
+              <p></p>
+          </div>
+
+              <div class="row"> 
+              <?php for ($i=0; $i < 6; $i++):?>            
+              <div class="column">
+                  <img class="demo cursor" src="img/<?php echo $row['bild']; ?>" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+                </div>
+            <?php endfor?>
+              </div>
+
+          </div>
+          <br>
+          <div class="row">
+
+            <div class="btn-group col-md-3">
+
+              <button class="produkt" id="minus">-</button>
+              <input type="text" maxlength="3" size="4" value="0"></input>
+              <button class="produkt"  id="plus">+</button>
+            
+          </div>
+          <div class="col-md-6"></div>
+          <div class="col-md-2 text-right">
+            <label style="font-size: larger;"><?php echo $row['preis']; ?> </label>  
+          </div>
+          <div class="col-md-1 text-right">
+            <label style="font-size: larger;">€ </label>  
+          </div>
+          </div> 
+          <br>
+        <div class="row"style="float: right;">
+            <div >
+                <button class="produkt" > In den Warenkorb</button> 
+              </div>  
+            </div>
+    </div>
+
+    <div class="col-md-4">
+      <h3 class="my-3"> <?php echo $row['name']; ?></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+      <h3 class="my-3">Project Details</h3>
+      <ul>
+        <li>Lorem Ipsum</li>
+        <li>Dolor Sit Amet</li>
+        <li>Consectetur</li>
+        <li>Adipiscing Elit</li>
+      </ul>
+    </div>
+  </div>
+<hr>
+<div>
+    <h2>Produkt Infos</h2>
+    <p><?php echo $row['beschreibung']; ?></p>
+    <hr>
+</div>
+
+
+<div >
+    <h2>Ähnliche Produkte</h2>
+    <div class="row" style="border:solid 1px grey">
+        <div class="col-md-3">
+          <a href="gtx1.php">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="img/1300282_0__72667.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-3">
+          <h3>GTX</h3>
+          <p>6GB MSI GeForce GTX 1660 ARMOR</p>
+        </div>
+        <br>
+      
+      <div class="col-md-3">
+        <a href="ram.php">
+          <img class="img-fluid rounded mb-3 mb-md-0" src="img/1237318_0__8850603.jpg" alt="">
+        </a>
+      </div>
+      <div class="col-md-3">
+        <h3>RAM</h3>
+        <p>16GB G.Skill Aegis DDR4-2666 DIMM CL19 Dual Kit</p>
+      </div>
+    </div>
+    </div>
+      <br>
+</div>
+
+
+<?php
+include ("footer.html");
+?>
+
+   <!-- Bootstrap core JavaScript -->
+   <script src="vendor/jquery/jquery.min.js"></script>
+   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
