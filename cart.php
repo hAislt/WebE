7ac989495fb2b532feb0d4de85595416f7d13387
hAislt <tranh@dhbw-loerrach.de>
2020-03-27 21:33:58 +0100

    Update cart.php
    - general rework of php connection
    - button redesigned and function added

--- cart.php
+++ cart.php
@@ -2,23 +2,47 @@
 error_reporting(-1);
 ini_set('display_errors','On');
  
-$mysqli = new mysqli('localhost', 'root', '', 'shop');
-if($mysqli->connect_error) {
-  echo 'Fehler bei der Verbindung: ' . mysqli_connect_error();
-  exit();
-  }
-  if(!$mysqli->set_charset('utf8')) {
-  echo 'Fehler beim Laden von UTF-8: ' . mysqli_error();
-  }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
   $sql = 'SELECT * FROM  products  ';
-  $result = $mysqli->query($sql);
+  $result = $conn->query($sql);
 
   $cardItems= (int)$result;
-  $mysqli->close();
+
+
+     
+  if(isset($_POST['button1'])) { 
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);  
+    $sql = "INSERT INTO cards (amount, product_id, user_id)
+  <--  VALUES BITTE SELBER  -->
+    VALUES ('2', '1234', '1')";
+
+if (mysqli_query($conn, $sql)) {
+    $last_id = mysqli_insert_id($conn);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+    
+    $conn->close();
+      
+  } 
+
+
 ?>
 
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 
   <head>
 
@@ -26,6 +50,7 @@ if($mysqli->connect_error) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="WebShop">
     <meta name="author" content="Mario">
+    <meta name="author" scripts="Hai Ha">
   
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -47,7 +72,7 @@ if($mysqli->connect_error) {
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Web Shop</a>
+      <a class="navbar-brand" href="index.php">Web Shop</a>
       <div class="collapse navbar-collapse" id="navbarMenu">
       <div class="input-group md-form form-sm form-2 pl-0">
         <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
@@ -120,15 +145,20 @@ if($mysqli->connect_error) {
                          <?php endwhile; ?>
                          <!-- END PRODUCT -->
                         
+
+                        
                      <div class="pull-right">
                          <a href="" class="btn btn-outline-secondary pull-right">
                              Update shopping cart
                          </a>
                      </div>
                  </div>
+                 
                  <div class="card-footer">
                      <div class="pull-right" style="margin: 10px">
-                         <a href="" class="btn btn-success pull-right">Buy</a>
+                         <form method="post" >
+                         <input type="submit" name="button1" class="btn btn-success pull-right"
+                            value="Buy"/>
                          <div class="pull-right" style="margin: 5px">
                              Total price: <b>50.00€</b>
                          </div>
