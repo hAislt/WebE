f1d8d6e13d16648d40e31fc965abf4c1dd22b500
hAislt <tranh@dhbw-loerrach.de>
2020-03-27 21:35:53 +0100

    Update contact.php
    - added link

--- contact.php
+++ contact.php
@@ -64,7 +64,7 @@ if(isset($_COOKIE['userId'])){
 <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Web Shop</a>
+      <a class="navbar-brand" href="index.php">Web Shop</a>
       <div class="input-group md-form form-sm form-2 pl-0">
         <input class="form-control my-0 py-1 red-border" type="text" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
